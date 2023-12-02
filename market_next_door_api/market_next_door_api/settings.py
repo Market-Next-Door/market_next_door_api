feae269b13c398b4b538420b968cc7163904832f
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-12-01 19:42:51 -0600

    updated debug line

--- market_next_door_api/market_next_door_api/settings.py
+++ market_next_door_api/market_next_door_api/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("secret_key")
+SECRET_KEY = 'django-insecure-p#foiyj5_)b8p2hm&qk4louxj+4q7)ox%m(2#!68z3jr$*+r5c'
+# os.environ.get('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
