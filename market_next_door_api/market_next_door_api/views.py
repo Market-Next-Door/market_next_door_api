684e6402910eb9a42af8119c107c356fefd38949
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-12-01 19:43:41 -0600

    add view for customers

--- market_next_door_api/market_next_door_api/views.py
+++ market_next_door_api/market_next_door_api/views.py
@@ -5,3 +5,13 @@ from rest_framework import status
 from .serializers import CustomerSerializer
 from .models import *
 
+@api_view(['GET', 'POST']) 
+def customer_list(request):
+
+  if request.method == 'GET':
+    customers = Customer.objects.all()
+    serializer = CustomerSerializer(customers, many=True)
+    return Response(serializer.data)
+  
+  if request.method == 'POST':
+    pass
