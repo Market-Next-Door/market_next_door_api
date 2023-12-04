332949eb1b796c624219e965d27222b36d70f608
Jorja Fleming <aset284@gmail.com>
2023-12-04 16:34:58 -0700

    Trying to deploy on circleci

--- market_next_door_api/market_next_door_api/serializers.py
+++ market_next_door_api/market_next_door_api/serializers.py
@@ -41,16 +41,3 @@ class PreorderSerializer(serializers.ModelSerializer):
   class Meta:
     model = Preorder
     fields = ['id', 'customer', 'item', 'ready']
-  
-  def to_representation(self, instance):
-    representation = super().to_representation(instance)
-    data = {
-      "id": representation['id'],
-      "type": "preorder",
-      "attributes": {
-        "customer": representation['customer'],
-        "item": representation['item'],
-        "ready": representation['ready']
-      }
-    }
-    return data
