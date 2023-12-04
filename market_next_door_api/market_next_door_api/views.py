b27371856bd5e96f4f6ce8319203d98cba6cb78f
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-04 17:55:26 -0600

    Create and update the preorder CRUD functions

--- market_next_door_api/market_next_door_api/views.py
+++ market_next_door_api/market_next_door_api/views.py
@@ -48,61 +48,7 @@ def customer_details(request, id):
 
 # Item CRUD functions (SRP)
 @api_view(['GET', 'POST'])
-def item_list(request):
-  if request.method == 'GET':
-    return get_item_list(request)
-  elif request.method == 'POST':
-    return create_item(request)
-
-def get_item_list(request):
-  items = Item.objects.all()
-  serializer = ItemSerializer(items, many=True)
-  return Response(serializer.data)
-
-def create_item(request):
-  serializer = ItemSerializer(data=request.data)
-  if serializer.is_valid():
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def item_details(request, id):
-  item = get_item_object(id)
-
-  if request.method == 'GET':
-    return get_item_details(item)
-  elif request.method == 'PUT':
-    return update_item(item, request.data)
-  elif request.method == 'DELETE':
-    return delete_item(item)
-
-def get_item_object(item_id):
-  try:
-    return Item.objects.get(pk=item_id)
-  except Item.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-def get_item_details(item):
-  serializer = ItemSerializer(item)
-  return Response(serializer.data)
-
-def update_item(item, data):
-  item_data = ItemSerializer(item, data=data)
-  if item_data.is_valid():
-    item_data.save()
-    return Response(item_data.data)
-  return Response(item_data.errors, status=status.HTTP_400_BAD_REQUEST)
-
-def delete_item(item):
-  item.delete()
-  return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-# Specific vendor's items CRUD functions (SRP)
-@api_view(['GET', 'POST'])
-def vendor_item_list(request, vendor_id):
+def item_list(request, vendor_id):
   try:
     vendor = Vendor.objects.get(pk=vendor_id)
   except Vendor.DoesNotExist:
@@ -126,7 +72,7 @@ def create_item(request):
   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def vendor_item_details(request, vendor_id, item_id):
+def item_details(request, vendor_id, item_id):
   try:
     vendor = Vendor.objects.get(pk=vendor_id)
   except Vendor.DoesNotExist:
@@ -138,24 +84,24 @@ def vendor_item_details(request, vendor_id, item_id):
     return Response(status=status.HTTP_404_NOT_FOUND)
   
   if request.method == 'GET':
-    return get_vendor_item_details(item)
+    return get_item_details(item)
   elif request.method == 'PUT':
-    return update_vendor_item(item, request.data)
+    return update_item(item, request.data)
   elif request.method == 'DELETE':
-    return delete_vendor_item(item)
+    return delete_item(item)
   
-def get_vendor_item_details(item):
+def get_item_details(item):
   serializer = ItemSerializer(item)
   return Response(serializer.data)
 
-def update_vendor_item(item, data):
+def update_item(item, data):
   item_data = ItemSerializer(item, data=data)
   if item_data.is_valid():
     item_data.save()
     return Response(item_data.data)
   return Response(status=status.HTTP_400_BAD_REQUEST)
 
-def delete_vendor_item(item):
+def delete_item(item):
   item.delete()
   return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -250,4 +196,65 @@ def update_market(market, data):
 
 def delete_market(market):
   market.delete()
+  return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+# Preorder CRUD functions (SRP)
+@api_view(['GET', 'POST'])
+def preorder_list(request, customer_id):
+  try:
+    check_customer = Customer.objects.get(pk=customer_id)
+  except Customer.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+  if request.method == 'GET':
+    return get_preorder_list(request, check_customer)
+  elif request.method == 'POST':
+    return create_preorder(request)
+
+def get_preorder_list(request, check_customer):
+  preorders = Preorder.objects.filter(customer=check_customer)
+  serializer = PreorderSerializer(preorders, many=True)
+  return Response(serializer.data)
+
+def create_preorder(request):
+  serializer = PreorderSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def preorder_details(request, customer_id, preorder_id):
+  try:
+    customer = customer.objects.get(pk=customer_id)
+  except customer.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  try:
+    preorder = Preorder.objects.get(pk=preorder_id, customer=customer)
+  except Preorder.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    return get_preorder_details(preorder)
+  elif request.method == 'PUT':
+    return update_preorder(preorder, request.data)
+  elif request.method == 'DELETE':
+    return delete_preorder(preorder)
+  
+def get_preorder_details(preorder):
+  serializer = PreorderSerializer(preorder)
+  return Response(serializer.data)
+
+def update_preorder(preorder, data):
+  preorder_data = PreorderSerializer(preorder, data=data)
+  if preorder_data.is_valid():
+    preorder_data.save()
+    return Response(preorder_data.data)
+  return Response(status=status.HTTP_400_BAD_REQUEST)
+
+def delete_preorder(preorder):
+  preorder.delete()
   return Response(status=status.HTTP_204_NO_CONTENT)
