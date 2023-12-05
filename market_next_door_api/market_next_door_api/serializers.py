1ae4cfe7d5d329cb37922304bd4a358ff3ef5c01
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-05 15:20:26 -0600

    Update the serializers to inlcude code that will display the assocaited object for each association

--- market_next_door_api/market_next_door_api/serializers.py
+++ market_next_door_api/market_next_door_api/serializers.py
@@ -5,19 +5,21 @@ from .models import *
 class CustomerSerializer(serializers.ModelSerializer):
   class Meta:
     model = Customer
-    fields = ['id', 'first_name', 'last_name', 'phone', 'email', 'location', 'date_created', 'updated_at']
+    fields = ['id', 'first_name', 'last_name', 'phone', 'email', 'password', 'location', 'date_created', 'updated_at']
 
-# Items
-class ItemSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Item
-    fields = ['id', 'item_name', 'vendor', 'price', 'size', 'quantity', 'availability', 'description', 'image', 'date_created', 'updated_at']
-  
 # Vendors
 class VendorSerializer(serializers.ModelSerializer):
   class Meta:
     model = Vendor
-    fields = ['id', 'market', 'vendor_name', 'first_name', 'last_name', 'email', 'location', 'date_created', 'updated_at']
+    fields = ['id', 'market', 'vendor_name', 'first_name', 'last_name', 'email', 'password', 'location', 'date_created', 'updated_at']
+
+# Items
+class ItemSerializer(serializers.ModelSerializer):
+  vendor = VendorSerializer(read_only=True)
+
+  class Meta:
+    model = Item
+    fields = ['id', 'item_name', 'vendor', 'price', 'size', 'quantity', 'availability', 'description', 'image', 'date_created', 'updated_at']
 
 # Markets
 class MarketSerializer(serializers.ModelSerializer):
@@ -27,6 +29,9 @@ class MarketSerializer(serializers.ModelSerializer):
 
 # Preorders
 class PreorderSerializer(serializers.ModelSerializer):
+  item = ItemSerializer(read_only=True)
+  customer = CustomerSerializer(read_only=True)
+
   class Meta:
     model = Preorder
     fields = ['id', 'customer', 'item', 'ready', 'date_created', 'updated_at']
