ceeaeba5f15655eab31209ea9ce2cd1a9ca3646a
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-12-05 22:01:51 -0600

    circle update

--- market_next_door_api/settings.py
+++ market_next_door_api/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 # import django_heroku 
 
@@ -88,6 +89,12 @@ DATABASES = {
         'PORT': '5432',        
     }
 }
+
+if 'CIRCLECI' in os.environ:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
